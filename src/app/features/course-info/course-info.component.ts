7a7da96a1c803ca72bd5c189f41795577105045c
Roman Portianko <rportyanko10@gmail.com>
2025-03-31 21:39:30 +0300

    implemented course info component

--- src/app/features/course-info/course-info.component.ts
+++ src/app/features/course-info/course-info.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CourseInfoComponent {
   // Use the names for the input `course`.
+  title = '';
+  description = '';
+  id = '';
+  creationDate: Date = new Date();
+  duration = 0;
+  authors: string[] = [];
 }
