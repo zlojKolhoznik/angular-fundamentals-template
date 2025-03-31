d4cabaf4772fd4b034926e25795492f883a10715
Roman Portianko <rportyanko10@gmail.com>
2025-03-31 21:42:16 +0300

    implemented course info module

---
+++ src/app/features/course-info/course-info.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseInfoComponent } from './course-info.component';
+
+
+
+@NgModule({
+  declarations: [CourseInfoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CourseInfoModule { }
