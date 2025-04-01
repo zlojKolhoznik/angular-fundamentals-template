9bf66dba7d4f1463f7dbf74f9c2c5d2473fb5acc
Roman Portianko <rportyanko10@gmail.com>
2025-04-01 16:14:16 +0300

    implemented custom date pipe

--- src/app/shared/pipes/custom-date.pipe.ts
+++ src/app/shared/pipes/custom-date.pipe.ts
@@ -1,8 +1,21 @@
-import { Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
     name: 'customDate'
 })
-export class CustomDatePipe {
-    // Add your code here
+export class CustomDatePipe implements PipeTransform {
+    transform(value: any, ...args: any[]) {
+        if (!(value instanceof Date)) {
+            value = new Date(value);
+        }
+
+        var day = value.getDate();
+        var month = value.getMonth() + 1; // Months are zero-based
+        var year = value.getFullYear();
+        return `${this.formatDatePart(day)}.${this.formatDatePart(month)}.${year}`;
+    }
+
+    formatDatePart(part: number): string {
+        return part < 10 ? '0' + part : part.toString();
+    }
 }
