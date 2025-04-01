df73efc544066da0c10d180e99f1473ce5f9a4c0
Roman Portianko <rportyanko10@gmail.com>
2025-04-01 14:49:04 +0300

    implemented registration form

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorDirective } from '@app/shared/directives/email.directive';
 
 @Component({
   selector: 'app-registration-form',
@@ -7,6 +8,10 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./registration-form.component.scss'],
 })
 export class RegistrationFormComponent {
-  registrationForm!: FormGroup;
+  registrationForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl('', [Validators.required, new EmailValidatorDirective().validate]),
+    password: new FormControl('', Validators.required),
+  });
   // Use the names `name`, `email`, `password` for the form controls.
 }
