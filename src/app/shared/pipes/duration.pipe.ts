fe15d0f7bce65bd1a295398c71fd40176b97e19d
Roman Portianko <rportyanko10@gmail.com>
2025-04-01 15:54:00 +0300

    implemented duration pipe

--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -1,8 +1,16 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
     name: 'duration'
 })
-export class DurationPipe {
-    // Add your code here
+export class DurationPipe implements PipeTransform {
+    transform(value: any, ...args: any[]) {
+        if (typeof value !== 'number') {
+            throw new Error('Invalid value type. Expected a number.');
+        }
+
+        const hours = Math.floor(value / 60);
+        const minutes = value % 60;
+        return `${hours}:${minutes} Hours`;
+    }
 }
