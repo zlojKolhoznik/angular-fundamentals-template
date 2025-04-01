383e8cf18734065a694ed6221c2d7094a98032b4
Roman Portianko <rportyanko10@gmail.com>
2025-04-01 14:19:52 +0300

    implemented email validator directive

--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -1,9 +1,33 @@
-import { Directive } from "@angular/core";
+import { Directive, OnChanges, SimpleChanges } from "@angular/core";
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from "@angular/forms";
 
 @Directive({
     selector: '[emailValidator]',
-    providers: [/*Add your code here*/]
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: EmailValidatorDirective,
+        multi: true
+    }]
 })
-export class EmailValidatorDirective {
+export class EmailValidatorDirective implements Validator, OnChanges {
+    private onChange?: () => void;
+    private emailPattern: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (this.onChange) {
+            this.onChange();
+        }
+    }
+
+    validate(control: AbstractControl): ValidationErrors | null {
+        const value = control.value;
+        return value && !this.emailPattern.test(value)
+        ? { invalidEmail: true }
+        : null;
+    }
+
+    registerOnValidatorChange?(fn: () => void): void {
+        this.onChange = fn;
+    }
     // Add your code here
 }
