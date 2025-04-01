196e3734d68c9526275b901a01e921b545c229d7
Roman Portianko <rportyanko10@gmail.com>
2025-04-01 16:08:27 +0300

    implemented create/edit course form

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {
-  FormBuilder, FormGroup
+  FormArray,
+  FormBuilder, FormGroup,
+  Validators
 } from '@angular/forms';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -13,7 +15,45 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 export class CourseFormComponent {
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
     library.addIconPacks(fas);
+    this.courseForm = this.createForm();
   }
   courseForm!: FormGroup;
+  initialAuthors: string[] = [];
   // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
+
+  private createForm(): FormGroup {
+    return this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(2)]],
+      description: ['', [Validators.required, Validators.minLength(2)]],
+      author: ['', [Validators.required, Validators.minLength(2), Validators.pattern(/^[a-zA-Z0-9 ]+$/)]],
+      authors: this.fb.array(['']),
+      duration: [0, [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  removeAuthorFromCourse(author: string) {
+    const index = this.initialAuthors.indexOf(author);
+    if (index !== -1) {
+      let courseAuthors = this.courseForm.get('authors') as FormArray;
+      courseAuthors.removeAt(index);
+    }
+
+    this.initialAuthors.push(author);
+  }
+
+  addAuthorToCourse(author: string) {
+    const index = this.initialAuthors.indexOf(author);
+    if (index !== -1) {
+      let courseAuthors = this.courseForm.get('authors') as FormArray;
+      courseAuthors.push(this.fb.control(author));
+      this.initialAuthors.splice(index, 1);
+    }
+  }
+
+  removeAuthorFromInitialAuthors(author: string) {
+    const index = this.initialAuthors.indexOf(author);
+    if (index !== -1) {
+      this.initialAuthors.splice(index, 1);
+    }
+  }
 }
