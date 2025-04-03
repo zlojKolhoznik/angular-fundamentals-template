2c0edb2f19c808da795f676e053852371e702e67
Roman Portianko <rportyanko10@gmail.com>
2025-04-03 13:17:37 +0300

    Refactor SessionStorageService to remove Window injection and use global window object directly

--- src/app/auth/services/session-storage.service.ts
+++ src/app/auth/services/session-storage.service.ts
@@ -1,4 +1,4 @@
-import {inject, Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 const TOKEN = 'SESSION_TOKEN'; // Use this constant for the session storage entry key
 // Add your code here
@@ -8,19 +8,19 @@ const TOKEN = 'SESSION_TOKEN'; // Use this constant for the session storage entr
 })
 export class SessionStorageService {
 
-  constructor(private window: Window) {
+  constructor() {
 
   }
 
   setToken(token: string){
-    this.window.sessionStorage.setItem(TOKEN, token);
+    window.sessionStorage.setItem(TOKEN, token);
   }
 
   getToken(){
-    return this.window.sessionStorage.getItem(TOKEN);
+    return window.sessionStorage.getItem(TOKEN);
   }
 
   deleteToken(){
-    this.window.sessionStorage.removeItem(TOKEN);
+    window.sessionStorage.removeItem(TOKEN);
   }
 }
