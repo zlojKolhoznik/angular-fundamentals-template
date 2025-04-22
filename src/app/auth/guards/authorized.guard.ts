d8562c6b017b6796da7056e8831effbf689c5809
Roman Portianko <rportyanko10@gmail.com>
2025-04-22 12:24:22 +0300

    Refactor AuthorizedGuard to simplify authorization logic and remove unnecessary RxJS operators

--- src/app/auth/guards/authorized.guard.ts
+++ src/app/auth/guards/authorized.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
-import { catchError, map, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -8,17 +8,11 @@ import { AuthService } from '../services/auth.service';
 })
 export class AuthorizedGuard implements CanLoad {
     constructor(private authService: AuthService, private router: Router) { }
-    canLoad(route: Route, segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        return this.authService.isAuthorized$.pipe(
-            map((isAuthorized) => {
-                if (!isAuthorized) {
-                    return this.router.createUrlTree(['/login']);
-                }
-                return true;
-            }),
-            catchError(() => {
-                return of(this.router.createUrlTree(['/login']));
-            })
-        );
+    canLoad(_route: Route, _segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        if (this.authService.isAuthorised) {
+            return true;
+        }
+
+        return this.router.createUrlTree(['/login']);
     }
 }
