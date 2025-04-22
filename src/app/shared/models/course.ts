54fdf533f662efa22b99010dfab54cb987babbf8
Roman Portianko <rportyanko10@gmail.com>
2025-04-22 12:54:11 +0300

    Fix creationDate type in Course interface from Date to string

--- src/app/shared/models/course.ts
+++ src/app/shared/models/course.ts
@@ -1,7 +1,7 @@
 export interface Course {
     title: string;
     description: string;
-    creationDate: Date;
+    creationDate: string;
     duration: number;
     authors: string[];
     id: string;
